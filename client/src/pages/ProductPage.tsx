82a4296ba2ee22ae93aac27d60845ebea806b213
ajowyang <andrewowyang875@gmail.com>
2024-10-25 00:10:55 +0000

    changed paragraph text color

--- client/src/pages/ProductPage.tsx
+++ client/src/pages/ProductPage.tsx
@@ -5,7 +5,7 @@ export function ProductPage() {
     <>
       <div className="text-white flex flex-col items-center mb-20">
         <h1 className="text-3xl mt-2">PRODUCTS</h1>
-        <p className=" w-96 text-center my-2">
+        <p className=" w-96 text-center my-2 text-gray-400">
           All of our jewelry is made from either solid .925 Sterling Silver or
           solid Stainless steel
         </p>
